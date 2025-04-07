2eb2fc5700be017ff10a9cee9f302d9dff3fbce6
Tranhieu305 <trunghieu2000.tdn@gmail.com>
2025-04-07 17:38:59 +0700

    Solution for 1732

---
+++ 1732. Find the Highest Altitude/Solution1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._1732._Find_the_Highest_Altitude
+{
+    class Solution1
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int highestAltitude = 0;
+            int currentAltitude = 0;
+
+            foreach (int change in gain)
+            {
+                currentAltitude += change;
+                highestAltitude = Math.Max(currentAltitude, highestAltitude);
+            }
+            return highestAltitude;
+        }
+    }
+}
