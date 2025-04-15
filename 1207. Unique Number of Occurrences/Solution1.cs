c4134e8917154854f8c4aad75c51c76636154820
hieu.tran <trunghieu2000.tdn@gmail.com>
2025-04-15 08:29:12 +0700

    Solution for 1207

---
+++ 1207. Unique Number of Occurrences/Solution1.cs
@@ -0,0 +1,28 @@
+﻿namespace Test._1207._Unique_Number_of_Occurrences
+{
+    internal class Solution1
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            var occurrences = new Dictionary<int, int>();
+            
+            foreach (int i in arr)
+            {
+                if (!occurrences.ContainsKey(i))
+                {
+                    occurrences[i] = 0;
+                }
+                occurrences[i]++;
+            }
+
+            var uniqueOccurrences = new HashSet<int>();
+            foreach (int occurr in occurrences.Values)
+            {
+                if (uniqueOccurrences.Contains(occurr))
+                    return false;
+                uniqueOccurrences.Add(occurr);
+            }
+            return true;
+        }
+    }
+}
