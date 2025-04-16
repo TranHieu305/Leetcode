2109190822bbc60015f88d3d01b7a6c7f682b7cc
hieu.tran <trunghieu2000.tdn@gmail.com>
2025-04-16 14:10:35 +0700

    Solution for 649

---
+++ 0933. Number of Recent Calls/RecentCounter.cs
@@ -0,0 +1,22 @@
+﻿namespace Test._0933._Number_of_Recent_Calls
+{
+    public class RecentCounter
+    {
+        private Queue<int> _pingQueue;
+        public RecentCounter()
+        {
+            _pingQueue = new Queue<int>();
+        }
+
+        public int Ping(int t)
+        {
+            _pingQueue.Enqueue(t);
+            return countPing(t - 3000, t);
+        }
+
+        private int countPing(int timeStart, int timeEnd)
+        {
+            return _pingQueue.Where(p => p >= timeStart && p <= timeEnd).Count();
+        }
+    }
+}
