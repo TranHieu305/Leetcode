d0c8affa467a1c20e8eb31e07069cda1cec0455b
hieu.tran <trunghieu2000.tdn@gmail.com>
2025-05-07 16:45:42 +0700

    Solution for 0002

---
+++ 0002. Add Two Numbers/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace Test._0002._Add_Two_Numbers
+{
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode head = new ListNode(0);
+            ListNode curr = head;
+            var carry = 0;
+
+            while (l1 != null || l2 != null || carry > 0) {
+                var sum = carry;
+
+                if (l1 != null)
+                {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                curr.next = new ListNode(sum % 10);
+                carry = sum / 10;
+                
+                curr = curr.next;
+            }
+
+            head = head.next;
+            return head;
+        }
+    }
+}
