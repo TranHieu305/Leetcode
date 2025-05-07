6959d8ad6694220604923a1627b41ccfe18bec95
hieu.tran <trunghieu2000.tdn@gmail.com>
2025-05-07 15:31:06 +0700

    Solution for 0009

---
+++ 0009. Palindrome Number/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace Test._0009._Palindrome_Number
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+            var str = x.ToString();
+
+            return string.Equals(str, string.Join("", str.Reverse()));
+        }
+    }
+}
