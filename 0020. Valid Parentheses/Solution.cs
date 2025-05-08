517d7776382f6761033a3fc238cd8f0a9038027e
hieu.tran <trunghieu2000.tdn@gmail.com>
2025-05-08 09:56:13 +0700

    Solution for 0020

---
+++ 0020. Valid Parentheses/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace Test._0020._Valid_Parentheses
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            var openBrackets = new List<char>() { '[', '{', '(' };
+            var dictClostBrackets = new Dictionary<char, char>()
+            {
+                { ']', '[' },
+                { '}', '{' },
+                { ')', '(' }
+            };
+
+            foreach (char bracket in s)
+            {   
+                // Push open bracket to stack
+                if (openBrackets.Contains(bracket))
+                {
+                    stack.Push(bracket);
+                }
+                else 
+                {
+                    var stackNotEmpty = stack.TryPop(out char top);
+                    if (!stackNotEmpty ||  top != dictClostBrackets[bracket]) return false;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
