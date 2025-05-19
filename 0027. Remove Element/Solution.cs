6fdf820e3aff20c75596d40e44565cd412d74512
hieu.tran <trunghieu2000.tdn@gmail.com>
2025-05-19 09:50:39 +0700

    Solution for 27

---
+++ 0027. Remove Element/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace Test._0027._Remove_Element
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int writeIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[writeIndex++] = nums[i];
+                }
+            }
+            return writeIndex;
+        }
+    }
+}
