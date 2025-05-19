e961cba23423e3d6c217c16363344154322a5237
hieu.tran <trunghieu2000.tdn@gmail.com>
2025-05-19 09:46:46 +0700

    Solution for 26

---
+++ 0026. Remove Duplicates from Sorted Array/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace Test._0026._Remove_Duplicates_from_Sorted_Array
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return 1;
+            var writeIndex = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[writeIndex])
+                {
+                    nums[++writeIndex] = nums[i];
+                }
+            }
+
+            return writeIndex + 1;
+
+        }
+    }
+}
