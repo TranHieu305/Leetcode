fe31cb8e109240cb0ceb8977d882a096006fe2bb
Tranhieu305 <trunghieu2000.tdn@gmail.com>
2025-09-25 01:49:23 +0000

    Solution for 0001

---
+++ 0001. Two Sum/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace Test._0001._Two_Sum;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            if (map.ContainsKey(complement))
+            {
+                return [map[complement], i];
+            }
+            else
+            {
+                map[nums[i]] = i;
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+}
