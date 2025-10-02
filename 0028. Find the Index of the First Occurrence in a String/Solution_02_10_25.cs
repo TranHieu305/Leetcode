4eefbe51bdd111d7cc33b0ce15d9f94c0bedd396
Tranhieu305 <trunghieu2000.tdn@gmail.com>
2025-10-02 09:16:44 +0000

    Solution for 0028 - cs, js

---
+++ 0028. Find the Index of the First Occurrence in a String/Solution_02_10_25.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode._0028._Find_the_Index_of_the_First_Occurrence_in_a_String;
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        return haystack.IndexOf(needle);
+    }
+}
---
+++ 0028. Find the Index of the First Occurrence in a String/Solution_02_10_25.js
@@ -0,0 +1,8 @@
+﻿/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
